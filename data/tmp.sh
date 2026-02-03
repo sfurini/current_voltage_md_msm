400d007f1dd7cfa5f968db3fdaf32531566a020e
Simone Furini <furini.simone@gmail.com>
2026-02-03 11:26:02 +0000

    Remove temp files

--- data/tmp.sh
+++
@@ -1,8 +0,0 @@
-cp /mnt/qnap/TMP/ef.-100mV.dtrajs.npz	5vk6.-100mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.100mV.dtrajs.npz	5vk6.100mV.dtrajs.npz
-cp /mnt/qnap/TMP/ef.-200mV.dtrajs.npz	5vk6.-200mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.200mV.dtrajs.npz	5vk6.200mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.250mV.dtrajs.npz	5vk6.250mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.300mV.dtrajs.npz	5vk6.300mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.350mV.dtrajs.npz	5vk6.350mV.dtrajs.npz
-cp  /mnt/qnap/TMP/ef.400mV.dtrajs.npz	5vk6.400mV.dtrajs.npz
